71dcfefd0eaf77b2b4b22be044349c1c834a028e
sellnat77 <sell_nat@yahoo.com>
2014-10-02 23:46:41 -0700

    Added main menu layout, has BG and buttons, no functionality yet

--- LifeAppV1/gen/com/lifeappv1/R.java
+++ LifeAppV1/gen/com/lifeappv1/R.java
@@ -23,9 +23,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int pyramid=0x7f020001;
+        public static final int simple_bg=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int main_emergency_button=0x7f080004;
+        public static final int main_hygiene_button=0x7f080003;
+        public static final int main_relationship_button=0x7f080002;
+        public static final int main_scripts_button=0x7f080000;
+        public static final int main_time_button=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,9 +41,13 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int main_emergency_button=0x7f050006;
+        public static final int main_hygiene_button=0x7f050005;
+        public static final int main_relationship_button=0x7f050004;
+        public static final int main_scripts_button=0x7f050002;
+        public static final int main_time_button=0x7f050003;
     }
     public static final class style {
         /** 

3cc893209021b9fe4fadf5938f449fc2c95431f5
Russell <sell_nat@yahoo.com>
2014-10-14 14:39:25 -0700

    Fixed my build path issues, needs to be checked n other machines

--- LifeAppV1/src/com/lifeappv1/ScriptsMainMenu.java
+++ LifeAppV1/src/com/lifeappv1/ScriptsMainMenu.java
@@ -2,8 +2,6 @@ package com.lifeappv1;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class ScriptsMainMenu extends ActionBarActivity {
 

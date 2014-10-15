ad29a0ec77b58d821d9ca3109999df70bc471a71
Edgar Cobos <ecobos@edcobos.com>
2014-10-15 10:59:44 -0700

    Fixed #23

--- LifeAppV1/src/com/lifeappv1/RelationshipMainMenu.java
+++ LifeAppV1/src/com/lifeappv1/RelationshipMainMenu.java
@@ -1,6 +1,9 @@
 package com.lifeappv1;
 
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.app.Activity;
 import android.os.Bundle;
 
@@ -10,5 +13,28 @@ public class RelationshipMainMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_relationship_main_menu);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.action_bar_search, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            //openSearch();
+	            return true;
+	        case R.id.action_settings:
+	            //openSettings();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
 }

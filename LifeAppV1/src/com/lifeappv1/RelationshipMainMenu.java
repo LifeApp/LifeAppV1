4d7ed79e082b594d620080ea1252e0e5e5e65515
Russell <sell_nat@yahoo.com>
2014-10-15 10:13:00 -0700

    Fixed runtime errors, for real this time

--- LifeAppV1/src/com/lifeappv1/RelationshipMainMenu.java
+++ LifeAppV1/src/com/lifeappv1/RelationshipMainMenu.java
@@ -1,9 +1,10 @@
 package com.lifeappv1;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class RelationshipMainMenu extends ActionBarActivity {
+public class RelationshipMainMenu extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
--- LifeAppV1/src/com/lifeappv1/ScriptsMainMenu.java
+++ LifeAppV1/src/com/lifeappv1/ScriptsMainMenu.java
@@ -1,9 +1,10 @@
 package com.lifeappv1;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class ScriptsMainMenu extends ActionBarActivity {
+public class ScriptsMainMenu extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
